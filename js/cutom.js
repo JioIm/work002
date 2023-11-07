914f315b1f68e641c9f4803309dcc34b10a30773
Jio Im <148739315+JioIm@users.noreply.github.com>
2023-11-07 18:08:38 +0900

    Add files via upload

--- js/cutom.js
+++ js/cutom.js
@@ -2,6 +2,19 @@ $(function (){
     $('.VisualSlide').slick({
         arrows: false,
         autoplay: true,
-        dots: true,
+        dots: false,
+    });
+
+    $('.main_product_slide').slick({
+        slidesToShow: 5,
+        arrows: false,
+        dots: true
+    });
+
+    $('.MainProduct .arrows .left').on('click', function(){
+        $('.main_product_slide').slick('slickPrev');
+    });
+    $('.MainProduct .arrows .right').on('click', function(){
+        $('.main_product_slide').slick('slickNext');
     });
 })
